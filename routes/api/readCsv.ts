554320f883a342e9df63d0a14512ff21c375e8c1
Hai.Nguyen <haingdc@gmail.com>
2024-09-17 22:12:20 +0700

    update path

--- routes/api/readCsv.ts
+++ routes/api/readCsv.ts
@@ -2,7 +2,7 @@
 import { readCsvFileContent } from "../../lib/read_file.ts";
 
 export default async function handler(req: Request) {
-  const posts = await readCsvFileContent('/Users/hainguyen/Documents/temp/blog_utils_very_fresh/downloads/alphabet.csv');
+  const posts = await readCsvFileContent('downloads/alphabet.csv');
   return new Response(JSON.stringify(posts), {
     headers: { "Content-Type": "application/json" },
   });
